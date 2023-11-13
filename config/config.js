c5acc5bdfd455997da76eec4b45a454d7e21422e
Naufal Alief <naufalalief087@gmail.com>
2023-11-13 18:23:33 +0700

    edit confing

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "123456",
+    database: process.env.DB_NAME || "db_sqlze",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || "3306",
     dialect: "mysql",
   },
   test: {
