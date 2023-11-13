0fae115afa6130a16d0c9cdccff5706e632698c6
Naufal Alief <naufalalief087@gmail.com>
2023-11-13 18:34:17 +0700

    s

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "123456",
-    database: process.env.DB_NAME || "db_sqlze",
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || "3306",
-    dialect: "mysql",
-  },
-  test: {
     username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    password: "-cbHCbE-65GD-Hec6CeB541fgebdE1cE",
+    database: "railway",
+    host: "monorail.proxy.rlwy.net",
+    port: "36865",
     dialect: "mysql",
   },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+  // test: {
+  //   username: "root",
+  //   password: null,
+  //   database: "database_test",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql",
+  // },
+  // production: {
+  //   username: "root",
+  //   password: null,
+  //   database: "database_production",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql",
+  // },
 };
